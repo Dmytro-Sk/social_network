477e82adee08c7dcfe12bb79e7c735847bbc8804
Dmytro-Sk <skorbatiuk@gmail.com>
2020-08-13 19:44:36 +0300

    Connect pictures to profiles
    - install pip Pillow
    - connect our pictures to profiles in users\models.py
    - add opportunity in user\admin.py to change profiles in admin panel
    - create folder .\media\profile_pics
    - add changes in settings.py
    - add changes in social_network\urls.py
    - create users\signals.py
    - add changes in users\apps.py
    - add changes in users\forms.py
    - add changes in users\views.py
    - add changes in users\templates\users\profile.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home_page'
 LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
