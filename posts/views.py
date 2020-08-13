3e8385ad9bc017fc39daac78d10ab2e0a46720f6
Dmytro-Sk <skorbatiuk@gmail.com>
2020-08-13 11:36:04 +0300

    Connect to database
    - connect our information from posts to database in models.py
    - make change in view.py
    - make change time format
    - add opportunity in admin.py to change posts in admin panel

--- posts/views.py
+++ posts/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 
-# todo This is temporary block
 def home_page(request):
     context = {
-            'posts': posts
+            'posts': Post.objects.all()
         }
     return render(request, 'posts/home_page.html', context)
-
-
-# todo This is temporary block
-posts = [
-    {
-        'author': 'User1',
-        'title': 'Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 12, 2020'
-    },
-    {
-        'author': 'User 2',
-        'title': 'Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 13, 2020'
-    }
-]
