39dd8fbd1fd0f04cfec29cb4fee6191ee883c892
Dmytro-Sk <skorbatiuk@gmail.com>
2020-08-13 14:36:28 +0300

    Create user registration
    - add users app
    - add users\templates\users\register.html
    - add users\forms.py
    - add registration urls
    - install pip crispy forms and add crispy_forms changes into settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'posts',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
